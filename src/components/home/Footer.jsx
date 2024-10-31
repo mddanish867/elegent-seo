50f9f7d9a3ff03949dbb7217e55a9090474e06d8
mddanish867 <mddanish867@gmail.com>
2024-10-31 23:24:07 +0530

    remodified these page

--- src/components/home/Footer.jsx
+++ src/components/home/Footer.jsx
@@ -1,20 +1,70 @@
 const Footer = () => {
   return (
     <>
-    <footer className="bg-blue-950 text-white py-12 px-4 sm:px-6 lg:px-8">
+      <footer className="bg-blue-950 text-white py-12 px-4 sm:px-6 lg:px-8">
         <div className="md:container mx-auto">
           <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
             <div>
-              <h3 className="text-lg font-serif mb-4 text-orange-300">Elegance SEO</h3>
-              <p className="text-gray-400">Elevating brands through sophisticated SEO strategies.</p>
+              <h3 className="text-lg font-serif mb-4 text-orange-300">
+                Elegance SEO
+              </h3>
+              <p className="text-gray-400">
+                Elevating brands through sophisticated SEO strategies.
+              </p>
             </div>
             <div>
               <h4 className="text-lg font-semibold mb-4">Quick Links</h4>
               <ul className="space-y-2">
-                <li><a href="#" className="text-gray-400 hover:text-orange-300 transition duration-300">Home</a></li>
-                <li><a href="#" className="text-gray-400 hover:text-orange-300 transition duration-300">Services</a></li>
-                <li><a href="#" className="text-gray-400 hover:text-orange-300 transition duration-300">About Us</a></li>
-                <li><a href="#" className="text-gray-400 hover:text-orange-300 transition duration-300">Contact</a></li>
+                <li>
+                  <a
+                    href="/"
+                    className="text-gray-400 hover:text-orange-300 transition duration-300"
+                  >
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="/services"
+                    className="text-gray-400 hover:text-orange-300 transition duration-300"
+                  >
+                    Services
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="/about"
+                    className="text-gray-400 hover:text-orange-300 transition duration-300"
+                  >
+                    About Us
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="/contact"
+                    className="text-gray-400 hover:text-orange-300 transition duration-300"
+                  >
+                    Contact
+                  </a>
+                </li>
+                <li>
+                  {" "}
+                  <a
+                    href="/process"
+                    className="text-white hover:text-orange-300 transition duration-300"
+                  >
+                    Process
+                  </a>
+                </li>
+                <li>
+                  {" "}
+                  <a
+                    href="/journey"
+                    className="text-white hover:text-orange-300 transition duration-300"
+                  >
+                    Journey
+                  </a>
+                </li>
               </ul>
             </div>
             <div>
@@ -25,9 +75,24 @@ const Footer = () => {
             <div>
               <h4 className="text-lg font-semibold mb-4">Follow Us</h4>
               <div className="flex space-x-4">
-                <a href="#" className="text-gray-400 hover:text-orange-300 transition duration-300">LinkedIn</a>
-                <a href="#" className="text-gray-400 hover:text-orange-300 transition duration-300">Twitter</a>
-                <a href="#" className="text-gray-400 hover:text-orange-300 transition duration-300">Instagram</a>
+                <a
+                  href="#"
+                  className="text-gray-400 hover:text-orange-300 transition duration-300"
+                >
+                  LinkedIn
+                </a>
+                <a
+                  href="#"
+                  className="text-gray-400 hover:text-orange-300 transition duration-300"
+                >
+                  Twitter
+                </a>
+                <a
+                  href="#"
+                  className="text-gray-400 hover:text-orange-300 transition duration-300"
+                >
+                  Instagram
+                </a>
               </div>
             </div>
           </div>
@@ -37,7 +102,7 @@ const Footer = () => {
         </div>
       </footer>
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/home/Navbar.jsx
+++ src/components/home/Navbar.jsx
@@ -1,33 +1,66 @@
-import { useNavigate } from "react-router-dom"
-
+import { Link, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
   const handleConsultant = () => {
-    navigate("/login")
-  }
+    navigate("/login");
+  };
+
   return (
     <>
-     <header className="py-6 px-4 sm:px-6 lg:px-8 bg-blue-950">
+      <header className="sticky top-0 z-50 py-6 px-4 sm:px-6 lg:px-8 bg-blue-950">
         <div className="md:container mx-auto flex justify-between items-center">
-          <div className="text-2xl font-serif text-orange-300">Elegance</div>      
+          <Link to="/">
+            <div className="text-2xl font-serif text-orange-300">Elegance</div>
+          </Link>
           <nav className="hidden md:flex space-x-8">
-            <a href="/" className="text-white hover:text-orange-300 transition duration-300">Home</a>
-            <a href="/services" className="text-white hover:text-orange-300 transition duration-300">Services</a>
-            <a href="/process" className="text-white hover:text-orange-300 transition duration-300">Process</a>
-            <a href="/journey" className="text-white hover:text-orange-300 transition duration-300">Journey</a>
-            <a href="/about" className="text-white hover:text-orange-300 transition duration-300">About</a>
-            <a href="/contact" className="text-white hover:text-orange-300 transition duration-300">Contact</a>
+            <a
+              href="/"
+              className="text-white hover:text-orange-300 transition duration-300"
+            >
+              Home
+            </a>
+            <a
+              href="/services"
+              className="text-white hover:text-orange-300 transition duration-300"
+            >
+              Services
+            </a>
+            <a
+              href="/process"
+              className="text-white hover:text-orange-300 transition duration-300"
+            >
+              Process
+            </a>
+            <a
+              href="/journey"
+              className="text-white hover:text-orange-300 transition duration-300"
+            >
+              Journey
+            </a>
+            <a
+              href="/about"
+              className="text-white hover:text-orange-300 transition duration-300"
+            >
+              About
+            </a>
+            <a
+              href="/contact"
+              className="text-white hover:text-orange-300 transition duration-300"
+            >
+              Contact
+            </a>
           </nav>
           <button
-          onClick={handleConsultant}
-          className="border border-orange-300 text-orange-300 hover:bg-orange-300 hover:text-blue-950 px-4 py-2 rounded transition duration-300 font-semibold">
+            onClick={handleConsultant}
+            className="border border-orange-300 text-orange-300 hover:bg-orange-300 hover:text-blue-950 px-4 py-2 rounded transition duration-300 font-semibold"
+          >
             Consultation
           </button>
         </div>
       </header>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
